b9a8d8e7dc6c5cbdcf1232457c6a1e30d603526b
AbiAman <135865180+AbiAman@users.noreply.github.com>
2024-05-06 03:15:15 +0300

    Update couponCtrl.js

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -1,5 +1,5 @@
 const Coupon = require("../models/couponModel");
-const validateMongoDbId = require("../utils/validateMongodbId");
+const validateMongoDbId = require("../utils/validateMongoDbId");
 const asynHandler = require("express-async-handler");
 
 const createCoupon = asynHandler(async (req, res) => {
