6061ff830bef569676057cceb7484a991d0e695e
AbiAman <135865180+AbiAman@users.noreply.github.com>
2024-05-06 03:15:04 +0300

    Update colorCtrl.js

--- controller/colorCtrl.js
+++ controller/colorCtrl.js
@@ -1,6 +1,6 @@
 const Color = require("../models/colorModel");
 const asyncHandler = require("express-async-handler");
-const validateMongoDbId = require("../utils/validateMongodbId");
+const validateMongoDbId = require("../utils/validateMongoDbId");
 
 const createColor = asyncHandler(async (req, res) => {
   try {
