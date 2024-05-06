2302e08237246339b28eac32dc2ed5fc3bbdcfe9
AbiAman <135865180+AbiAman@users.noreply.github.com>
2024-05-06 03:14:32 +0300

    Update blogCatCtrl.js

--- controller/blogCatCtrl.js
+++ controller/blogCatCtrl.js
@@ -1,6 +1,6 @@
 const Category = require("../models/blogCatModel");
 const asyncHandler = require("express-async-handler");
-const validateMongoDbId = require("../utils/validateMongodbId");
+const validateMongoDbId = require("../utils/validateMongoDbId");
 
 const createCategory = asyncHandler(async (req, res) => {
   try {
